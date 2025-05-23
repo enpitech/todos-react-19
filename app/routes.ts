b68151f2f291227c0e9b8b9c4fec34ff6d14d777
nir2002 <nir.parisian@gmail.com>
2025-05-23 18:08:35 +0300

    home route

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,6 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import { type RouteConfig, route } from "@react-router/dev/routes";
 
 export default [
-  index("routes/home.tsx"),
+  route("/home", "routes/home.tsx"),
   route("/todos/:id?", "routes/todos.tsx"),
 ] satisfies RouteConfig;
