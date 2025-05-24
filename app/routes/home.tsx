2aa7f50dd599c63393beec43316aec3063a7c8c8
nir2002 <nir.parisian@gmail.com>
2025-05-25 00:49:08 +0300

    not in use

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,39 +1,10 @@
-// https://react.dev/blog/2024/04/25/react-19#whats-new-in-react-19
-// Fetch todos ✅
-// update todo ✅
-// show completed todos ✅
-// Race condition ✅
-// Throw an error - what will happen? ✅
-// startTransition ✅
-// useTransition - supporting async and error boundary - https://react.dev/reference/react/useTransition# - ✅
-
-// Create new Todo - ✅
-// display error - ✅
-// formAction - ✅
-// useActionState - React query -> Meta frameworks -> Primitive! - https://react.dev/reference/react/useActionState#noun-labs-1201738-(2) - ✅
-// return error - ✅
-// loading state with useFormStatus - ✅
-// Improve update UX/Performance - ✅
-// useOptimistic - https://react.dev/reference/react/useOptimistic#noun-labs-1201738-(2) - ✅
-
 // use + Suspense - https://react.dev/reference/react/use#noun-labs-1201738-(2)
 
 // Meta tags - https://react.dev/reference/react-dom/components/meta#noun-labs-1201738-(2)
 // Performance - preFetchDNS, preconnect, preinit, preInitModule, preload, preloadModule
 
-import {
-  Suspense,
-  use,
-  useActionState,
-  useEffect,
-  useOptimistic,
-  useState,
-  useTransition,
-} from "react";
 import { Todo, type TodoProps } from "~/components/Todo";
 import "../app.css";
-import { useFormStatus } from "react-dom";
-import { useNavigation, type ActionFunctionArgs } from "react-router";
 import { sleep } from "~/utils";
 import { ErrorBoundary } from "react-error-boundary";
 
