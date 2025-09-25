aa560a076e0316b03ef27f7ae05f1a08b450bd44
nir2002 <nir.parisian@gmail.com>
2025-09-25 12:51:51 +0300

    new starting point

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -26,7 +26,7 @@
 // fetch('/todos', {method: 'PATCH'}) - Update existing todo
 // fetch('/todos', {method: 'POST'}) - Create new todo
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Todo } from "~/components/Todo";
 
 export type TodoData = {
@@ -37,10 +37,34 @@ export type TodoData = {
 
 export default function Home() {
   const [todos, setTodos] = useState<TodoData[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    setError("");
+    setLoading(true);
+    fetch("/todos")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch todos");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setTodos(data);
+        setLoading(false);
+      })
+      .catch((e) => setError(e.toString()))
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
 
   return (
     <div className="App">
       <div className="Todos">
+        {loading && <div>Loading...</div>}
+        {error && <div className="text-red-500">Error: {error}</div>}
         <form>
           {todos.map((todo) => {
             return (
@@ -56,28 +80,6 @@ export default function Home() {
     </div>
   );
 }
-
-// Fetch todos
-
-// useEffect(() => {
-//     setError("");
-//     setLoading(true);
-//     fetch("/todos")
-//       .then((response) => {
-//         if (!response.ok) {
-//           throw new Error("Failed to fetch todos");
-//         }
-//         return response.json();
-//       })
-//       .then((data) => {
-//         setTodos(data);
-//         setLoading(false);
-//       })
-//       .catch((e) => setError(e.toString()))
-//       .finally(() => {
-//         setLoading(false);
-//       });
-//   }, []);
 
 // Update todo
 
